e536e20bcb67cd18beb38360d07da0a361a43238
amanmaurya7 <amaurya.dev@gmail.com>
2025-09-23 00:02:26 +0530

    Refactor project for Exercise Tracker API

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
